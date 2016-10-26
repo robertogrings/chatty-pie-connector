3a69be9cfc6843ee827075f7131e7a92b7d5448e
EmilDafinov <emil.dafinov@appdirect.com>
2016-10-26 15:57:32 -0400

    refactoring to match changes in the SDK

--- src/main/java/com/chattypie/RootConfiguration.java
+++ src/main/java/com/chattypie/RootConfiguration.java
@@ -5,16 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import com.appdirect.sdk.ConnectorSdkConfiguration;
-import com.appdirect.sdk.isv.IsvSpecificMarketplaceCredentials;
-import com.appdirect.sdk.isv.IsvSpecificMarketplaceCredentialsSupplier;
+import com.appdirect.sdk.appmarket.IsvSpecificAppmarketCredentials;
+import com.appdirect.sdk.appmarket.IsvSpecificAppmarketCredentialsSupplier;
 
 @Configuration
 @Import(ConnectorSdkConfiguration.class)
 public class RootConfiguration {
     
     @Bean
-    public IsvSpecificMarketplaceCredentialsSupplier credentialsSupplier() {
-        return () -> new IsvSpecificMarketplaceCredentials("sample", "sample");
+    public IsvSpecificAppmarketCredentialsSupplier credentialsSupplier() {
+        return () -> new IsvSpecificAppmarketCredentials("sample", "sample");
     }
 }
 
